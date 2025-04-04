b509f5b236134b845aa7a1d05b01f9952843302f
Gonzalo Contreras <gonzacd9@gmail.com>
2025-04-04 15:09:15 +0200

    chatbot

--- js/vf_conf.js
+++ js/vf_conf.js
@@ -1,17 +1,16 @@
-export async function getVoiceflowConfig() {
-    const http = "https://portafolio-dvhu.vercel.app/";
-
-    try {
-        const response = await fetch(http + "vf-config");
-        if (response.ok) {
-            const config = await response.json();
-            return config;  // Devuelve la configuración de Voiceflow
-        } else {
-            console.error("Error al obtener la configuración de Voiceflow.");
-            return null;
-        }
-    } catch (error) {
-        console.error("Error al hacer la solicitud:", error);
-        return null;
-    }
-}
+// Cargar el widget de Voiceflow con la configuración obtenida desde la API
+(function(d, t) {
+  var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+  v.onload = function() {
+    window.voiceflow.chat.load({
+      verify: { projectID: '{{voiceflow.projectid}}' },
+      url: '{{voiceflow.url}}',
+      versionID: '{{voiceflow.versionid}}',
+      voice: {
+        url: "{{voiceflow.voiceurl}}"
+      }
+    });
+  }
+  v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+})(document, 'script');
+  
