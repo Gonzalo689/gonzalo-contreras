bde393b7142fabae39cd74fc78629ee80cba5eef
Gonzalo Contreras <gonzacd9@gmail.com>
2025-04-05 13:49:30 +0200

    .

--- js/chatbot.js
+++ js/chatbot.js
@@ -4,7 +4,6 @@
     v.onload = function() {
       window.voiceflow.chat.load({
       verify: { projectID: '67ed530006ac7129ab8d5a49'},
-      apiKey: 'VF.DM.67efbc926a9e92d44dd9bc1a.VM2XfU67EBcVK2PA',
       url: 'https://general-runtime.voiceflow.com',
       versionID: 'production',
       voice: {
